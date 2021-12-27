c4f60e0291dece655099beff9a4ea9c1d7bff30d
Yakov Chibizov <chibizov96@gmail.com>
2021-12-27 21:09:58 +0300

    Commit 3
    Task Done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] Random(int[] arr, int min, int max)
+{
+    for (int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(min, max);
+    Console.WriteLine("Базовый массив: " + String.Join(" ", arr));
+    return arr;
+}
+
+int[] Method(int[] arr, int[] arrB)
+{
+    Console.WriteLine("Четные элементы массива: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arrB[i] += arr[i];
+            Console.Write(arrB[i] + " ");
+        }
+    }
+    return (arrB);
+}
+
+Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] Array1 = new int[length];
+int[] Array2 = new int[Array1.Length];
+Random(Array1, 0, 100);
+Method(Array1, Array2);
